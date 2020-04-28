35d3584e28d83222b97486d4fbeefe7c1209242d
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-04-28 22:14:04 +0200

    display creation time on list

--- src/components/tracks/SingleTrack/SingleTrack.tsx
+++ src/components/tracks/SingleTrack/SingleTrack.tsx
@@ -12,9 +12,18 @@ interface Props {
 
 const SingleTrack: FunctionComponent<Props> = (props: Props) => {
   const { track, removeTrack } = props;
+
+  let creationText: string = "";
+
+  if (track.creationTime) {
+    const creationDate = new Date(track.creationTime);
+    creationText = `${creationDate.getFullYear()}. ${creationDate.getMonth()}. ${creationDate.getDate()}. ${creationDate.getHours()}:${creationDate.getMinutes()}`;
+  }
+
   return (
     <div className={styles.track}>
       <h3 className={styles.name}>{track.name}</h3>
+      <p>{creationText}</p>
       <Button clicked={() => removeTrack(track.id)} state={ButtonState.danger}>
         Delete
       </Button>
