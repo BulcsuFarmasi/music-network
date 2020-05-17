da70c2142622ca8c30855b36fcab1da482ff10c9
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-05-17 19:06:55 +0200

    add loading to auth state

--- src/models/state/auth-state.ts
+++ src/models/state/auth-state.ts
@@ -1,6 +1,8 @@
+import { LoadingState } from "./loading-state";
 import { User } from "../user";
 
 export interface AuthState {
   authed: boolean;
+  loading: LoadingState;
   loggedInUser?: User;
 }
--- src/store/reducers/auth.ts
+++ src/store/reducers/auth.ts
@@ -1,8 +1,10 @@
 import { AuthAction } from "../actions/creators/auth";
 import { AuthState } from "../../models/state/auth-state";
+import { LoadingState } from "../../models/state/loading-state";
 
 const initialState: AuthState = {
   authed: false,
+  loading: LoadingState.initial,
 };
 
 export const authReducer = (
