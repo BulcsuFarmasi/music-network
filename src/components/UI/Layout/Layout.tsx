b79fd1a648a17eb00aebe959bd1874cadf9dfa05
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-05-17 18:12:38 +0200

    hide menu while unauthed

--- src/components/UI/Layout/Layout.tsx
+++ src/components/UI/Layout/Layout.tsx
@@ -1,18 +1,31 @@
 import React, { FunctionComponent } from "react";
+import { connect } from "react-redux";
 
 import { Navigation } from "../Navigation/Navgiation";
 import Routes from "../../Routes/Routes";
+import { AppState } from "../../../models/state/app-state";
 
 import styles from "./Layout.module.scss";
 
-const Layout: FunctionComponent = () => {
+interface Props {
+  authed: boolean;
+}
+
+const Layout: FunctionComponent<Props> = (props: Props) => {
+  const { authed } = props;
   return (
     <div className={styles.layout}>
-      <Navigation />
+      {authed ? <Navigation /> : null}
       <h1>Music Network</h1>
       <Routes />
     </div>
   );
 };
 
-export default Layout;
+const mapStateToProps = (state: AppState) => {
+  return {
+    authed: state.auth.authed,
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
