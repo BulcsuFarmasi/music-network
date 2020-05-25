e308e8472bdefc40f19e85ef6e76e89396026144
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-05-25 20:44:35 +0200

    update reducer to handle register actions

--- src/store/reducers/auth.ts
+++ src/store/reducers/auth.ts
@@ -1,15 +1,36 @@
 import { AuthAction } from "../actions/creators/auth";
+import {
+  AUTH_REGISTER_START,
+  AUTH_REGISTER_SUCCESS,
+} from "../actions/types/auth";
 import { AuthState } from "../../models/state/auth-state";
 import { LoadingState } from "../../models/state/loading-state";
+import { updateObject } from "../../utils/object-utils";
 
 const initialState: AuthState = {
   authed: false,
   loading: LoadingState.initial,
 };
 
+const authRegisterSuccess = (state: AuthState): AuthState => {
+  return updateObject(state, { loading: LoadingState.onGoing });
+};
+
+const startLoading = (state: AuthState): AuthState => {
+  return updateObject(state, { loading: LoadingState.completed });
+};
+
 export const authReducer = (
   state: AuthState = initialState,
   action: AuthAction
 ) => {
+  switch (action.type) {
+    case AUTH_REGISTER_START:
+      startLoading(state);
+      break;
+    case AUTH_REGISTER_SUCCESS:
+      authRegisterSuccess(state);
+      break;
+  }
   return state;
 };
