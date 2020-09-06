8c0cfb1bd1310c43ea107983a39628154687742e
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-09-06 10:30:18 +0200

    add author property to track

--- src/models/track.ts
+++ src/models/track.ts
@@ -1,11 +1,13 @@
 import { File } from "./file";
+import { Profile } from "./profile";
 
 export interface Track {
   id?: string;
   authorId?: string;
+  author?: Profile;
   creationTime?: number;
   name?: string;
   file?: File;
   likers?: string[];
-  [key: string]: string | string[] | undefined | number | File;
+  [key: string]: string | string[] | undefined | number | File | Profile;
 }
