0e3a2a3bfcd16d55136ff6e407ece427547cbca0
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-09-12 22:28:24 +0200

    (fix) toggle likers visiblity

--- src/components/Like/Likers/Likers.tsx
+++ src/components/Like/Likers/Likers.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 import { Profile } from "../../../models/profile";
 import { MiniProfile } from "../../Profile/MiniProfile/MiniProfile";
@@ -10,14 +10,24 @@ interface LikersProps {
 const Likers: FunctionComponent<LikersProps> = (props: LikersProps) => {
   const { likers } = props;
 
+  const [likersVisible, setLikersVisible] = useState<boolean>(false);
+
+  const toggleLikersVisible = () => {
+    setLikersVisible(!likersVisible);
+  };
+
   const likerList = likers.map((liker: Profile) => (
     <MiniProfile profile={liker}></MiniProfile>
   ));
 
+  const likerListContainer = likersVisible ? (
+    <div className="likers-list">{likerList}</div>
+  ) : null;
+
   return (
     <div className="likers">
-      <p>Likers</p>
-      {likerList}
+      <p onClick={toggleLikersVisible}>Likers</p>
+      {likerListContainer}
     </div>
   );
 };
