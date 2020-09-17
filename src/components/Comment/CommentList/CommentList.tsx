a3cdb4addfb360ae53fe5cc1e324f3a9e0f10bda
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-09-17 21:10:59 +0200

    use add comment component

--- src/components/Comment/CommentList/CommentList.tsx
+++ src/components/Comment/CommentList/CommentList.tsx
@@ -1,9 +1,12 @@
 import React, { FunctionComponent } from "react";
 
+import { AddComment } from "../AddComment/AddComment";
+
 export const CommentList: FunctionComponent = () => {
   return (
     <div>
       <p>Comments</p>
+      <AddComment></AddComment>
     </div>
   );
 };
