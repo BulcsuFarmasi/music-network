7863a04db5e155d9c6ab5077ab2ea1239f040c85
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-09-17 21:09:05 +0200

    create add comment component

---
+++ src/components/Comment/AddComment/AddComment.tsx
@@ -0,0 +1,9 @@
+import React, { FunctionComponent } from "react";
+
+export const AddComment: FunctionComponent = () => {
+  return (
+    <div>
+      <textarea placeholder="Write something" />
+    </div>
+  );
+};
