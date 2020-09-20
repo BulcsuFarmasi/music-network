7baf0f97b7a11a16be9352a0a9095b92bdf5d668
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-09-20 20:36:59 +0200

    add comment action types

---
+++ src/store/actions/types/comment.ts
@@ -0,0 +1,9 @@
+export const ADD_COMMENT: string = "ADD_COMMENT";
+export const ADD_COMMENT_ERROR: string = "ADD_COMMENT_ERROR";
+export const ADD_COMMENT_START: string = "ADD_COMMENT_START";
+export const ADD_COMMENT_SUCCESS: string = "ADD_COMMENT_SUCCESS";
+
+export const FETCH_COMMENT: string = "FETCH_COMMENT";
+export const FETCH_COMMENT_ERROR: string = "FETCH_COMMENT_ERROR";
+export const FETCH_COMMENT_START: string = "FETCH_COMMENT_START";
+export const FETCH_COMMENT_SUCCESS: string = "FETCH_COMMENT_SUCCESS";
