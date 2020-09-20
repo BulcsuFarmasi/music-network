ec196bf3eed26819095fb282a3e480f793a39638
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-09-20 20:38:51 +0200

    add types to auth action types

--- src/store/actions/types/auth.ts
+++ src/store/actions/types/auth.ts
@@ -1,19 +1,19 @@
-export const AUTH_LOGIN = "AUTH_LOGIN";
-export const AUTH_LOGIN_START = "AUTH_LOGIN_START";
-export const AUTH_LOGIN_SUCCESS = "AUTH_LOGIN_SUCCESS";
+export const AUTH_LOGIN: string = "AUTH_LOGIN";
+export const AUTH_LOGIN_START: string = "AUTH_LOGIN_START";
+export const AUTH_LOGIN_SUCCESS: string = "AUTH_LOGIN_SUCCESS";
 
-export const AUTH_LOGOUT = "AUTH_LOGOUT";
+export const AUTH_LOGOUT: string = "AUTH_LOGOUT";
 export const AUTH_LOGOUT_SUCCESS = "AUTH_LOGOUT_SUCCES";
 
-export const AUTH_REGISTER = "AUTH_REGISTER";
-export const AUTH_REGISTER_START = "AUTH_REGISTER_START";
-export const AUTH_REGISTER_SUCCESS = "AUTH_REGISTER_SUCCESS";
+export const AUTH_REGISTER: string = "AUTH_REGISTER";
+export const AUTH_REGISTER_START: string = "AUTH_REGISTER_START";
+export const AUTH_REGISTER_SUCCESS: string = "AUTH_REGISTER_SUCCESS";
 
-export const AUTH_REFRESH = "AUTH_REFRESH";
+export const AUTH_REFRESH: string = "AUTH_REFRESH";
 
-export const CHECK_AUTH = "CHECK_AUTH";
+export const CHECK_AUTH: string = "CHECK_AUTH";
 
-export const UPDATE_PROFILE_PICTURE = "UPDATE_PROFILE_PICTURE";
+export const UPDATE_PROFILE_PICTURE: string = "UPDATE_PROFILE_PICTURE";
 
-export const UPDATE_USER = "UPDATE_USER";
-export const UPDATE_USER_SUCCESS = "UPDATE_USER_SUCCESS";
+export const UPDATE_USER: string = "UPDATE_USER";
+export const UPDATE_USER_SUCCESS: string = "UPDATE_USER_SUCCESS";
