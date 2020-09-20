217b5c618f7bfe9361e4979cc48d33536d5e6819
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-09-20 20:41:25 +0200

    add types to track action types

--- src/store/actions/types/track.ts
+++ src/store/actions/types/track.ts
@@ -1,22 +1,22 @@
-export const ADD_TRACK = "ADD_TRACK";
-export const ADD_TRACK_ERROR = "ADD_TRACK_ERROR";
-export const ADD_TRACK_START = "ADD_TRACK_START";
-export const ADD_TRACK_SUCCESS = "ADD_TRACK_SUCCESS";
+export const ADD_TRACK: string = "ADD_TRACK";
+export const ADD_TRACK_ERROR: string = "ADD_TRACK_ERROR";
+export const ADD_TRACK_START: string = "ADD_TRACK_START";
+export const ADD_TRACK_SUCCESS: string = "ADD_TRACK_SUCCESS";
 
-export const CLEAR_TRACK_ERROR = "CLEAR_TRACK_ERROR";
-export const CLEAR_TRACK_LOADING = "CLEAR_TRACK_LOADING";
+export const CLEAR_TRACK_ERROR: string = "CLEAR_TRACK_ERROR";
+export const CLEAR_TRACK_LOADING: string = "CLEAR_TRACK_LOADING";
 
-export const DELETE_TRACK = "DELETE_TRACK";
-export const DELETE_TRACK_ERROR = "DELETE_TRACK_ERROR";
-export const DELETE_TRACK_START = "DELETE_TRACK_START";
-export const DELETE_TRACK_SUCCESS = "DELETE_TRACK_SUCCESS";
+export const DELETE_TRACK: string = "DELETE_TRACK";
+export const DELETE_TRACK_ERROR: string = "DELETE_TRACK_ERROR";
+export const DELETE_TRACK_START: string = "DELETE_TRACK_START";
+export const DELETE_TRACK_SUCCESS: string = "DELETE_TRACK_SUCCESS";
 
-export const FETCH_TRACK = "FETCH_TRACK";
-export const FETCH_TRACK_ERROR = "FETCH_TRACK_ERROR";
-export const FETCH_TRACK_START = "FETCH_TRACK_START";
-export const FETCH_TRACK_SUCCESS = "FETCH_TRACK_SUCCESS";
+export const FETCH_TRACK: string = "FETCH_TRACK";
+export const FETCH_TRACK_ERROR: string = "FETCH_TRACK_ERROR";
+export const FETCH_TRACK_START: string = "FETCH_TRACK_START";
+export const FETCH_TRACK_SUCCESS: string = "FETCH_TRACK_SUCCESS";
 
-export const UPDATE_TRACK = " UPDATE_TRACK";
-export const UPDATE_TRACK_ERROR = " UPDATE_TRACK_ERROR";
-export const UPDATE_TRACK_START = " UPDATE_TRACK_START";
-export const UPDATE_TRACK_SUCCESS = " UPDATE_TRACK_SUCCESS";
+export const UPDATE_TRACK: string = " UPDATE_TRACK";
+export const UPDATE_TRACK_ERROR: string = " UPDATE_TRACK_ERROR";
+export const UPDATE_TRACK_START: string = " UPDATE_TRACK_START";
+export const UPDATE_TRACK_SUCCESS: string = " UPDATE_TRACK_SUCCESS";
